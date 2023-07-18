2e324f6949337e6c7d73f20ecde1fdb2b4561af3
Weder Ribas <me@wederribas.com>
2023-07-17 17:14:41 -0700

    Make nvimtree open to the right side

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -2,4 +2,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    view = {
+        side = "right",
+        width = 40
+    }
+})
+
